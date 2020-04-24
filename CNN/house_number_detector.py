1c6a45fe8ee18260848b3d620ff6e0ce8434e9b0
Terry <tthurk3@gatech.edu>
2020-04-24 17:48:35 -0500

    working on non maximum supression

--- CNN/house_number_detector.py
+++ CNN/house_number_detector.py
@@ -7,9 +7,14 @@ import logging as log
 
 
 class HouseNumberDetector(object):
-    def __init__(self, input_dir=""):
+    def __init__(
+        self, input_dir="", tf_model_dir="", model_filename="CNNHouseNumbersModel.h5"
+    ):
         """class constructor of house number detector"""
         self.images = list()
+        self.cnn = tf.keras.models.load_model(
+            os.path.join(self.tf_model_dir, model_filename)
+        )
 
     def denoise_img(self, img):
         """
@@ -56,15 +61,7 @@ class HouseNumberDetector(object):
             self.images.append(img)
 
     def get_mser_regions(
-        self,
-        img,
-        min_area=15,
-        max_area=75,
-        delta=20,
-        min_diversity=5000,
-        padding_h=6,
-        padding_w=4,
-        visualize=True,
+        self, img, min_area=15, max_area=75, delta=20, min_diversity=10000,
     ):
         """
         find regions in images where the is likely to be a number
@@ -93,31 +90,74 @@ class HouseNumberDetector(object):
         for p in regions:
             x_max, y_max = np.amax(p, axis=0)
             x_min, y_min = np.amin(p, axis=0)
-            # apply padding to bounding box
-            x_max += padding_w // 2
-            x_min -= padding_w // 2
-            y_max += padding_h // 2
-            y_min -= padding_h // 2
             # add bounding boxes in a dictionary hashmap
             bounding_boxes.append(
                 {"x_max": x_max, "x_min": x_min, "y_min": y_min, "y_max": y_max}
             )
-            # flag for visualizing the MSER regions
-            if visualize:
-                COLOR = np.random.randint(0, high=255)
-                cv2.rectangle(
-                    img, (x_min, y_max), (x_max, y_min), (COLOR, COLOR, COLOR), 1
-                )
-
-        # flag for visualizing the MSER regions
-        if visualize:
-            cv2.imshow("img", img)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
 
         return bounding_boxes
+
+    def get_img_pyramid(
+        self,
+        img,
+        region,
+        w_scales=[1.6, 1.4, 1.2, 1.0, 0.8, 0.6],
+        h_scales=[1.4, 1.2, 1.0, 0.8, 0.6, 0.4],
+        visualize=True,
+    ):
+        """
+        generate bounding boxes of different scales for scale invariance
+        Args:
+            region(dict): dictionary of points of the original bounding box
+            w_scales(list): list of floating point scales to scale the bounding
+                            box width
+            h_scales(list): list of floating point scales to scale the bounding
+                            box height
+        returns (list): list of dictionaries of the rescaled bounding boxes
+        """
+        scaled_regions = list()
+        for i in range(len(h_scales)):
+            scaled_region = dict()
+            # width calculations
+            height = region["y_max"] - region["y_min"]
+            # CNN model is trained on a 32x32 square image
+            # so width is set equal to height
+            width = height
+
+            width *= w_scales[i]
+            scaled_region.update({"x_max": int(region["x_max"] + width / 2)})
+            scaled_region.update({"x_min": int(region["x_min"] - width / 2)})
+            scaled_regions.append(scaled_regions)
+            # height calculations
+            height *= h_scales[i]
+            scaled_region.update({"y_max": int(region["y_max"] + height // 2)})
+            scaled_region.update({"y_min": int(region["y_min"] - height // 2)})
+            # flag for visualizing the MSER regions
+            if visualize:
+                img = img.copy()
+                COLOR = np.random.randint(0, high=255)
+                cv2.rectangle(
+                    img,
+                    (scaled_region["x_min"], scaled_region["y_max"]),
+                    (scaled_region["x_max"], scaled_region["y_min"]),
+                    (COLOR, COLOR, COLOR),
+                    1,
+                )
+                cv2.imshow("img", img)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
+
+        return scaled_regions
+
+    def get_best_pred(self, img, regions):
+        """
+        perform non maximal supression to choose the best region
+        of prediction
+        """
+        # for region in regions:
 
     def detect_numbers(self):
         for img in self.images:
             regions = self.get_mser_regions(img)
-            
+            for region in regions:
+                scaled_regions = self.get_img_pyramid(img, region)
